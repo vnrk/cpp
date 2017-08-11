ddacd428e6fc203f3b4b8eebcc179c1aad6fcaea
vnrk <koppu.venkata@gmail.com>
2017-08-11 20:37:14 +0530

    initial

---
+++ MultipleInheritence.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Base{
+	public:
+	int base_var;
+};
+
+class Derived1:public virtual Base{};
+class Derived2:public virtual Base{};
+
+class Derived3:public Derived1,public Derived2{
+	public:
+	void method(){
+		base_var=1;
+	}
+};
+
+int main(){
+	Derived3 *d3=new Derived3();
+	//Base *b=d3;
+	return 0;
+}
