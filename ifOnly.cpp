c168528bb76b62f03cc3915b6aad04675c63b62a
vnrk <koppu.venkata@gmail.com>
2017-08-11 20:31:50 +0530

    initial

---
+++ ifOnly.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a=5,b=10; 
+	char x=1,y=0; 
+	if(a,b,x,y) 
+	cout<<"MAIN"<<endl;
+	return 0;
+}
---
+++ initialisation.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+static int x;
+class A{
+	public:
+	static int i;
+	A(int j){
+		i=j;
+	}
+};
+
+int A::i=8;
+
+int main(){
+	//A aObj;
+	//aObj.i=20;
+	A aObj(2);
+	x=10;
+	cout<< "i= "<<aObj.i<< endl;
+	cout<< "x= " << x;
+	return 0;
+}
+
+
