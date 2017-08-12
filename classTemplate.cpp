d41b1d48eac89ec88a30ffa22bde56b05fe5b797
vnrk <koppu.venkata@gmail.com>
2017-08-12 09:44:06 +0530

    initial

---
+++ classTemplate.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <class S>
+class classTemplate{
+public:
+	S x,y;
+	classTemplate(S a,S b){
+		x=a;
+		y=b;
+	}
+	S Bigger();
+};
+
+template <class S>
+S classTemplate<S>::Bigger(){
+	return x<y?y:x;
+}
+
+int main(){
+	class classTemplate <int> ct(332,444);
+	cout<< "Worked because of Class Template, Bigger Number is "<< ct.Bigger() << endl;
+	return 0;
+}
---
+++ codeSnippet.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class test {
+	public:
+	static int n;
+	test () { n++; };
+	~test () { n--; };
+};
+
+int test::n=0;
+
+int main () {
+	test a;
+	test b[5];
+	test *c = new test;
+	cout << a.n << endl;
+	delete c;
+	cout << test::n << endl;
+	return 0;
+}
