058f40eef2c6a3d3d72d463a0b4bf8cc28c76a85
vnrk <koppu.venkata@gmail.com>
2017-08-12 09:47:51 +0530

    initial

---
+++ ExceptionThrow.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class BaseEx{};
+class DerivedEx:public BaseEx{};
+
+void func2(){
+	DerivedEx dd;
+	throw dd;
+}
+
+void func1(){
+	try{ func2();}
+	catch(BaseEx ex) { throw ex; }
+}
+
+int main(){
+	try{func1();}
+	catch (DerivedEx ex){ cout << "DerivedEx"<< endl;}
+	catch (BaseEx ex){ cout << "BaseEx"<< endl;}
+	catch(...){cout << "UnKnown"<< endl;}
+}
+
